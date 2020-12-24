395b401a043103313236e98590c713f4f054a7ce
aaronwb <zmtzwb@gmail.com>
2020-12-24 21:26:25 +0800

    登录页，logger中间件添加

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const Route = require('@koa/router');
 const KoaStatic = require('koa-static');
 const njs = require('koa-nunjucks-2');
 const path = require('path');
+const bodyParser = require('koa-bodyparser');
+
+const logger = require('koa-logger')
+
 const config = require('../config/config');
 
 const blog = require('./store/blog');
@@ -23,6 +27,9 @@ app.use(async (ctx, next) => {
     ctx.state.blog = blog;
     return next();
 });
+
+app.use(logger());
+app.use(bodyParser());
 
 route.get('/', async ctx => {
     await ctx.render('index', {
@@ -49,7 +56,19 @@ route.get('/posts/:id', async ctx => {
 });
 
 route.get('/login.html', async ctx => {
+
     await ctx.render('login');
+});
+
+route.post('/login.html', async ctx => {
+    const {username, password} = ctx.request.body;
+    let status = false;
+    if (username === blog.username && password === blog.password) {
+        status = true;
+    }
+    return ctx.render('login-result', {
+        status
+    })
 });
 
 app.use(route.routes());
--- src/store/blog.js
+++ src/store/blog.js
@@ -3,4 +3,7 @@ module.exports = {
     location: '杭州',
     nickName: 'AaronW',
     avatar: 'http://aaronw.fastgo.vip/upload/2020/12/avatar-074c479b62e142c291bfcd730960a347.jpg',
+
+    username: "admin",
+    password: "admin"
 }
